697f10558278f4f8f1734470ae06f32d83d649a0
NDK <ndk@1200@gmail.com>
2024-09-10 00:25:31 +0700

    create comments on posts

---
+++ src/features/comment/CommentReaction.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CommentReaction() {
+  return <div></div>;
+}
+
+export default CommentReaction;
--- src/features/post/PostCard.js
+++ src/features/post/PostCard.js
@@ -1,7 +1,77 @@
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import {
+  Avatar,
+  Box,
+  Card,
+  CardHeader,
+  IconButton,
+  Link,
+  Stack,
+  Typography,
+} from "@mui/material";
 import React from "react";
+import { Link as RouterLink } from "react-router-dom";
+import { fDate } from "../../utils/formatTime";
+import CommentForm from "../comment/CommentForm";
+import CommentList from "../comment/CommentList";
+import PostReaction from "./PostReaction";
 
 function PostCard({ post }) {
-  return <div>{post.content}</div>;
+  return (
+    <Card>
+      <CardHeader
+        disableTypography
+        avatar={
+          <Avatar src={post?.author?.avatarUrl} alt={post?.author?.name} />
+        }
+        title={
+          <Link
+            variant="subtitle2"
+            color="text.primary"
+            component={RouterLink}
+            sx={{ fontWeight: 600 }}
+            to={`/user/${post.author._id}`}
+          >
+            {post?.author?.name}
+          </Link>
+        }
+        subheader={
+          <Typography
+            variant="caption"
+            sx={{ display: "block", color: "text.secondary" }}
+          >
+            {fDate(post.createdAt)}
+          </Typography>
+        }
+        action={
+          <IconButton>
+            <MoreVertIcon sx={{ fontSize: 30 }} />
+          </IconButton>
+        }
+      />
+
+      <Stack spacing={2} sx={{ p: 3 }}>
+        <Typography>{post.content}</Typography>
+
+        {post.image && (
+          <Box
+            sx={{
+              borderRadius: 2,
+              overflow: "hidden",
+              height: 300,
+              "& img": { objectFit: "cover", width: 1, height: 1 },
+            }}
+          >
+            <img src={post.image} alt="post" />
+          </Box>
+        )}
+
+        <PostReaction post={post} />
+        <CommentList postId={post._id} />
+        <CommentForm postId={post._id} />
+      </Stack>
+    </Card>
+  );
 }
 
 export default PostCard;
